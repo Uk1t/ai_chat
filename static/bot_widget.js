d5c6d8be6f78b6cde6f26a4972f092ee38b492d5
Uk1t <art.vvolf@gmail.com>
2026-03-20 12:54:28 +0300

    bags

--- static/bot_widget.js
+++ static/bot_widget.js
@@ -22,7 +22,7 @@
             const div = document.createElement("div");
             div.textContent = `${who}: ${text}`;
             div.style.color = "white" ;
-            div.style.maxWidth = '50%'
+            div.style.maxWidth = '85%'
             div.style.marginLeft = who ==="Вы" ? "auto" : "0"
             div.style.marginBottom = '5px'
             chat.appendChild(div);
